c4e5638eb2442b3990fc3fd12a1db054b5ed9092
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-06-30 10:08:19 -0700

    Initialized Project

---
+++ src/test/java/sftpClientTests/MainTests.java
@@ -0,0 +1,17 @@
+package test.java.sftpClientTests;
+
+public class MainTests {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        assert(true);
+    }
+}
