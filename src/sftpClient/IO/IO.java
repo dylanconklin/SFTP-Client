23b8d55ddd01f59b3e866987818d97b6b1ba17a7
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-02 00:48:44 -0700

    Add I/O Convenience Class

---
+++ src/sftpClient/IO/IO.java
@@ -0,0 +1,47 @@
+package sftpClient.IO;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class IO {
+    public static void write(ArrayList<String> text, File file) {
+        write(String.join("\n", text), file);
+    }
+
+   public static void write(String text, File file) {
+        try {
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(text);
+        } catch(Exception e) {
+        }
+    }
+
+    public static ArrayList<String> read(File file) {
+        ArrayList<String> result = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String input;
+            result = br.lines().collect(Collectors.toCollection(ArrayList::new));
+        } catch(Exception e) {
+        }
+        return result;
+    }
+
+    public static String read(File file, String delimiter) {
+        return String.join(delimiter, read(file));
+    }
+
+    public static String getInputFromUser(String prompt) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = "";
+        System.out.print(prompt);
+        try {
+            input = bufferedReader.readLine();
+        } catch(Exception e) {
+        }
+        return input;
+    }
+}
--- src/sftpClient/Main.java
+++ src/sftpClient/Main.java
@@ -1,9 +1,5 @@
 package sftpClient;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         REPL repl = new REPL();
