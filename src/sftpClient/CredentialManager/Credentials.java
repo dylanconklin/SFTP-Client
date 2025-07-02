0ce35a04fe0ff6eef3d6cb16f6e3993d73696e23
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-02 09:52:53 -0700

    Added JSON Support for Credentials

--- src/sftpClient/CredentialManager/Credentials.java
+++ src/sftpClient/CredentialManager/Credentials.java
@@ -1,5 +1,8 @@
 package sftpClient.CredentialManager;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Credentials {
     String host;
     int port;
@@ -18,5 +21,29 @@ public class Credentials {
         this.port = 0;
         this.username = "";
         this.password = "";
+    }
+
+    public String toJSON() {
+        JSONObject jo = new JSONObject();
+        jo.put("host", host);
+        jo.put("port", port);
+        jo.put("username", username);
+        jo.put("password", password);
+        return jo.toJSONString();
+    }
+
+    static Credentials fromJSON(String json) {
+        Credentials credentials = new Credentials();
+        try {
+            JSONObject jo = (JSONObject) new JSONParser().parse(json);
+            credentials = new Credentials(
+                    (String) jo.get("host"),
+                    (Integer) jo.get("port"),
+                    (String) jo.get("username"),
+                    (String) jo.get("password")
+            );
+        } catch (Exception e) {
+        }
+        return credentials;
     }
 }
