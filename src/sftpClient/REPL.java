944e974d98210eed2d9c2db73cdb317ee544bc87
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-04 03:07:15 -0700

    Update REPL to Use Intents

--- src/sftpClient/REPL.java
+++ src/sftpClient/REPL.java
@@ -1,7 +1,7 @@
 package sftpClient;
 
 import sftpClient.IO.IO;
-
+import sftpClient.Intent.Intent;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -22,57 +22,8 @@ public class REPL {
 
     public ArrayList<String> eval(String input) {
         ArrayList<String> output = new ArrayList<>();
-        String[] parts = input.trim().split("\\s+");
-        String command = parts[0];
-        switch (command) {
-            case "get":
-                if (parts.length < 2) {
-                    output.add("get Error: Missing Parameters Like File Names");
-                } else {
-                    String filename = parts[1];
-                    output.add("Downloading ....  " + filename + " .... Please Wait");
-                    // Get the Actual file here to download
-                    // Show Success of Fail
-                }
-                break;
-
-            case "put":
-                if (parts.length < 2) {
-                    output.add("Put Error: Missing Parameters Like File Names");
-                } else {
-                    String filename = parts[1];
-                    output.add("Uploading ....  " + filename + " .... Please Wait");
-                    // Upload the actual file here
-                    // Show Success of Fail
-                }
-                break;
-
-            case "ls":
-                output.add("Listing All the Files In the Current Directory....  ");
-                // Actually list all the files in the current directory
-                // Show Success of Fail
-                break;
-
-            case "rm":
-                if (parts.length < 2) {
-                    output.add("Rm Error: Missing Parameters Like File Names");
-                } else {
-                    String filename = parts[1];
-                    output.add("Deleting ....  " + filename + " .... Please Wait");
-                    // Acutally delete the files here
-                    // Show Success of Fail
-                }
-                break;
-
-            case "exit":
-                output.add("Exiting FTP client.");
-                break;
-
-            default:
-                output.add("Unknown Command: " + command);
-        }
-
-
+        Intent intent = Intent.getIntent(input);
+        intent.execute();
         return output;
     }
 
