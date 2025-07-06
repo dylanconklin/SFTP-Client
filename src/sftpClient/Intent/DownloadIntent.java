19bd021d67a6bf2e7dc4a39d4b2aeeec0d775f15
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-05 22:14:14 -0700

    Cleaned Up Layout of DownloadIntent

--- src/sftpClient/Intent/DownloadIntent.java
+++ src/sftpClient/Intent/DownloadIntent.java
@@ -4,24 +4,27 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import sftpClient.Client.Client;
 
 public class DownloadIntent extends Intent {
+    ArrayList<String> files;
+
     @Override
     void parse(ArrayList<String> args) {
         ArrayList<String> output = new ArrayList<>();
         if (args.size() < 2) {
             output.add("get Error: Missing Parameters Like File Names");
         }
+        files = new ArrayList<>(args.subList(1, args.size()));
     }
 
     @Override
     public ArrayList<String> execute(Client client, ArrayList<String> args) {
+        parse(args);
         ArrayList<String> output = new ArrayList<>();
-        new ArrayList<>(args.subList(1, args.size()))
-                .forEach( filename -> {
+        files
+                .forEach(filename -> {
                     File destFile = new File(filename);
                     try {
                         OutputStream dest = new FileOutputStream(destFile);
