0229457957faf1c74f4f1e6ad94bb7f6ab38277e
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-07-08 08:45:37 -0700

    Added ListIntent
    This could be temporary, I just needed this to start working on DeleteIntent

--- src/sftpClient/Intent/ListIntent.java
+++ src/sftpClient/Intent/ListIntent.java
@@ -1,6 +1,9 @@
 package sftpClient.Intent;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import com.jcraft.jsch.ChannelSftp;
 import sftpClient.Client.Client;
 
 public class ListIntent extends Intent {
@@ -10,10 +13,56 @@ public class ListIntent extends Intent {
 
     @Override
     public ArrayList<String> execute(Client client, ArrayList<String> args) {
-        ArrayList<String> output = new ArrayList<>();
-        output.add("Listing All the Files In the Current Directory....  ");
-        // Actually list all the files in the current directory
-        // Show Success of Fail
-        return null;
+        ArrayList<ChannelSftp.LsEntry> output = new ArrayList<>();
+        try {
+            String path = ".";
+
+            ArrayList<ChannelSftp.LsEntry> dirItems =
+                    new ArrayList<>(client.sftp.ls(path));
+
+            ArrayList<ChannelSftp.LsEntry> hiddenDirectories =
+                    dirItems
+                            .stream()
+                            .filter((e) -> e.getFilename().startsWith("."))
+                            .filter((e) -> e.getAttrs().isDir())
+                            .sorted()
+                            .collect(Collectors.toCollection(ArrayList::new));
+
+            ArrayList<ChannelSftp.LsEntry> hiddenFiles =
+                    dirItems
+                            .stream()
+                            .filter((e) -> e.getFilename().startsWith("."))
+                            .filter((e) -> !e.getAttrs().isDir())
+                            .sorted()
+                            .collect(Collectors.toCollection(ArrayList::new));
+
+            ArrayList<ChannelSftp.LsEntry> directories =
+                    dirItems
+                            .stream()
+                            .filter((e) -> !e.getFilename().startsWith("."))
+                            .filter((e) -> e.getAttrs().isDir())
+                            .sorted()
+                            .collect(Collectors.toCollection(ArrayList::new));
+
+            ArrayList<ChannelSftp.LsEntry> files =
+                    dirItems
+                            .stream()
+                            .filter((e) -> !e.getFilename().startsWith("."))
+                            .filter((e) -> !e.getAttrs().isDir())
+                            .sorted()
+                            .collect(Collectors.toCollection(ArrayList::new));
+
+            output.addAll(hiddenDirectories);
+            output.addAll(hiddenFiles);
+            output.addAll(directories);
+            output.addAll(files);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return output
+                .stream()
+                .map((e) -> e.getFilename() + (e.getAttrs().isDir() ? "/" : ""))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
